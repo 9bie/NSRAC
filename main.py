9842f993a03114758279c21cf18f717d871361c7
9bie <blackguwc@163.com>
2019-02-24 00:11:11 +0800

    ok~

--- main.py
+++ main.py
@@ -25,45 +25,80 @@ def string_middle(start_str, end, html):
 class BaiduPan(object):
     def __init__(self,Cookies=None):
         self.logind=False
+
         self.w = requests.session()
         self.headers = {
             "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "Accept-Encoding":"gzip, deflate, br",
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             "Accept-Language":"zh-HK,zh;q=0.9,ja-JP;q=0.8,ja;q=0.7,zh-CN;q=0.6,zh-TW;q=0.5,en-US;q=0.4,en;q=0.3",
             "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
+            'X-Requested-With': 'XMLHttpRequest',
         }
-        self.Cookies = Cookies
+        self.w.headers.update(self.headers)
+        self.w.cookies.update(Cookies)
         if Cookies:
             self.__verifyIsLogin()
 
 
-        self.share_verify_api="https://pan.baidu.com/share/verify?surl=%s&t=%s&channel=chunlei&web=1&app_id=250528&bdstoken=%s&logid=%s=&clienttype=0"
-        self.transfer_api = "https://pan.baidu.com/share/transfer?shareid=%s&from=%s&channel=chunlei&web=1&app_id=250528&bdstoken=%s&logid=%s&clienttype=0"
-
+        self.share_verify_api="https://pan.baidu.com/share/verify?surl=%s" \
+                              "&t=%s" \
+                              "&channel=chunlei" \
+                              "&web=1" \
+                              "&app_id=250528" \
+                              "&bdstoken=%s" \
+                              "&logid=%s" \
+                              "&clienttype=0"
+        self.transfer_api = "https://pan.baidu.com/share/transfer?shareid=%s" \
+                            "&from=%s" \
+                            "&channel=chunlei" \
+                            "&ondup=newcopy"\
+                            "&async=1"\
+                            "&web=1" \
+                            "&app_id=250528" \
+                            "&bdstoken=%s" \
+                            "&logid=%s" \
+                            "&clienttype=0"
+        self.report_user_api="https://pan.baidu.com/api/report/user?channel=chunlei&web=1&app_id=250528&bdstoken=%s" \
+                             "&logid=%s" \
+                             "&clienttype=0"
+        self.memship_api="https://pan.baidu.com/rest/2.0/membership/user?method=query" \
+                         "&reminder=1" \
+                         "&channel=chunlei" \
+                         "&web=1" \
+                         "&app_id=250528" \
+                         "&bdstoken=%s" \
+                         "&logid=%s&clienttype=0"
+        temp = "http://yun.baidu.com/share/transfer?shareid=%s&from=%s&bdstoken=%s&channel=chunlei&clienttype=0&web=1&app_id=250528"
     def __getLogid(self):
-        jsfunc='''var i = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/~£¡@#£¤%¡­¡­&", a = String.fromCharCode,o = function (e){if (e.length < 2) {var r = e.charCodeAt(0);return 128 > r ? e : 2048 > r ? a(192 | r >>> 6) + a(128 | 63 & r) : a(224 | r >>> 12 & 15) + a(128 | r >>> 6 & 63) + a(128 | 63 & r)}var r = 65536 + 1024 * (e.charCodeAt(0) - 55296) + (e.charCodeAt(1) - 56320);return a(240 | r >>> 18 & 7) + a(128 | r >>> 12 & 63)+ a(128 | r >>> 6 & 63) + a(128 | 63 & r)},u = /[\uD800-\uDBFF][\uDC00-\uDFFFF]|[^\x00-\x7F]/g, c = function (e){return (e +""+Math.random()).replace(u, o)}, l = function (e) {var r = [0, 2, 1][e.length % 3],t = e.charCodeAt(0) << 16 | (e.length > 1 ? e.charCodeAt(1) : 0) << 8 | (e.length > 2 ? e.charCodeAt(2) : 0),n = [i.charAt(t >>> 18), i.charAt(t >>> 12 & 63), r >= 2 ? "=" : i.charAt(t >>> 6 & 63), r >= 1 ? "=" : i.charAt(63 & t)];return n.join("")}, p = function (e) {return e.replace(/[\s\S]{1,3}/g, l)}, d = function () {return p(c((new Date).getTime()))}, g = function (e, r) {return r ? d(String(e)).replace(/[+\/]/g, function (e) {return "+" == e ? "-" : "_"}).replace(/=/g, "") : d(String(e))};'''
-        ctx = execjs.compile(jsfunc)
+        jsfunc = open("js.js","r")
+        ctx = execjs.compile(jsfunc.read())
+        jsfunc.close()
         return ctx.call('g',"baiduid")
     def __verifyIsLogin(self):
+        ret = self.w.get("https://passport.baidu.com/center")
+        open("temp.html", "wb").write(ret.content)
         self.logind=True
         # gugugu~
     def login(self,user,passwd):
         pass
-    def __getShareuk(self,source):return string_middle('uk":','"',source)
-    def __getFsidlist(self,source):return string_middle('fs_id":','"',source)
+    def __getShareuk(self,source):return string_middle('"uk":',',',source)
+    def __getFsidlist(self,source):return string_middle('"fs_id":',',',source)
     def __getBdstoken(self,source):return string_middle('bdstoken":"','"',source)
-    def __getShareid(self,source):return string_middle('shareid":"','"',source)
+    def __getShareid(self,source):return string_middle('"shareid":',',',source)
 
     def transfer(self,bdlink,code=None,path="/"):
         if not self.logind:
             print("Please login.")
             return False
-        obj = self.w.get(bdlink,headers=self.headers,cookies=self.Cookies)
+        obj = self.w.get(bdlink)
+        logid =self.__getLogid()
         bdstoken = self.__getBdstoken(source=obj.text)
-        if obj.status_code == 302 and not code:
+        if obj.status_code == 302 and not code or "请输入提取码".encode("utf-8") in obj.content and not code:
             print("Need Code.")
             return False
-        else:
+        elif "请输入提取码".encode("utf-8") in obj.content and code:
             if not bdstoken:
                 print("Get bdstoken Failed.")
                 return False
@@ -73,31 +108,56 @@ class BaiduPan(object):
                 "vcode" :"",
                 "vcode_str":""
             }
-            result = self.w.post(self.share_verify_api%(surl,int(time.time()),bdstoken,self.__getLogid()),headers=self.headers,cookies=self.Cookies,data=data)
+            self.w.headers.update({"referer":obj.url})
+            result = self.w.post(self.share_verify_api%(surl,(int(round(time.time() * 1000))),bdstoken,logid),data=data,
+                                 )
             r2 = json.loads(result.text)
             if "randsk" not in r2:
                 print("Get Randsk Faild.")
+                print(result.text)
                 return False
-            self.Cookies["SCRC"] = r2["randsk"]
+            self.w.cookies.update({"BDCLND":r2["randsk"]})
+
             # Verify Sussfully
-        obj = self.w.get(bdlink, headers=self.headers, cookies=self.Cookies)
+        obj = self.w.get(bdlink)
+
         fsidlist=self.__getFsidlist(obj.text)
         shareid=self.__getShareid(obj.text)
         formid=self.__getShareuk(obj.text)
+        self.w.get(self.memship_api%(bdstoken,logid))
+
+
+        self.w.headers.update({"origin": "https://pan.baidu.com"})
+        self.w.headers.update({"referer":bdlink})
+
+        self.w.post(self.report_user_api%(bdstoken,logid),data={
+            "timestamp":int(time.time()),
+            "action":"share_transfer"
+        })
         data = {
-            "fsidlist":quote("[%s]"%fsidlist),
-            "path":quote(path)
+            "fsidlist": [int(fsidlist)],
+            "path": '/'
         }
-        result = self.w.post(self.transfer_api%(shareid,formid,bdstoken,self.__getLogid()),data=data,headers=self.headers,cookies=self.Cookies)
+        print(data)
+        result = self.w.post(self.transfer_api%(shareid,formid,bdstoken,logid),data="fsidlist=%5B"+fsidlist+"%5D&path=%2F")
         r2 = json.loads(result.text)
-        if "error" not in r2:
+        if "errno" not in r2:
             print("Unknow Error.")
+            print(result.text)
             return False
         else:
-            if r2["error"] == "0":
+            if r2["errno"] == "0":
                 print("Transfer Successfully")
                 return True
             else:
                 print("Transfer Faild.")
                 print(result.text)
                 return False
+if __name__=="__main__":
+    a = BaiduPan({
+        "BAIDUID":":FG=1",
+        "STOKEN":"",
+        "BDUSS":"~~dKFz~3Shce",
+
+    })
+    a.transfer("https://pan.baidu.com/s/1vs78d9IvYDSs3WB3ixgCjA",code="uwoo")
